093b0030fe9bfa0c940f6157f9cfd17583ef0bbd
Alex <o.tantsiura@spd-ukraine.com>
2021-06-05 20:20:01 +0300

    ci
    Signed-off-by: Alex <o.tantsiura@spd-ukraine.com>

--- Dockerfile
+++ Dockerfile
@@ -3,5 +3,6 @@ FROM node:12-alpine
 WORKDIR /app
 COPY . /app
 RUN npm i
+EXPOSE 3000
 
 CMD [ "npm", "run", "build-start" ]
--- configs/docker-run.sh
+++ configs/docker-run.sh
@@ -1,3 +1,3 @@
-docker build -t rest:latest .
-docker rmi $(docker images -f dangling=true -q) -f
-docker run -d rest:latest -p 3000:3000
+docker build -t rest:latest ..
+docker rmi $(docker images -f dangling=true -q) -f || true
+docker run -d -p 3000:3000 rest:latest
